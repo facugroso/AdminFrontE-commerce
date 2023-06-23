6e51ad6e2da7d5e2c20650b472adada98c324cce
valentinalr <valentina.lerose@outlook.com>
2023-06-23 12:46:32 -0300

    .

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function NavBar() {
+  const user = useSelector((state) => state.user.dataValues);
+
+  console.log(user);
+
   return (
     <nav className="navbar bg-white mb-5">
       <div className="container-fluid d-flex justify-content-between">
@@ -8,7 +13,11 @@ function NavBar() {
           Admin Panel
         </Link>
         <div>
-          <i className="bi bi-person-circle" style={{ fontSize: "30px" }}></i>
+          <span>Welcome, {user.firstname}</span>
+          <i
+            className="bi bi-person-circle"
+            style={{ fontSize: "30px", marginLeft: "0.5rem" }}
+          ></i>
         </div>
       </div>
     </nav>
