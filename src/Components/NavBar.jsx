95c716b6057e7602b5ece67bf448ca3c536891cd
facugroso <110434971+facugroso@users.noreply.github.com>
2023-06-30 10:45:51 -0300

    dataValues

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
 
 function NavBar() {
-  const user = useSelector((state) => state.user.dataValues);
+  const user = useSelector((state) => state.user);
   return (
     <nav className="navbar bg-white mb-5">
       <div className="container-fluid d-flex justify-content-end align-items-center px-4">
-        <span>Welcome, {user.firstname}</span>
+        <span>Welcome, {user && user.dataValues.firstname}</span>
         <i
           className="bi bi-person-circle"
           style={{ fontSize: "30px", marginLeft: "0.5rem" }}
